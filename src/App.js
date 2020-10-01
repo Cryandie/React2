b57710557d901ba936d45b1bf62ddfd1e4454b11
Seif <seifbensghaier@gmail.com>
2020-10-01 17:35:45 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className={"title red"}>Seïf</h1>
+
+        <br></br>
+
+        <img src="/image1.png" alt=""></img>
+
+        <br></br>
+
+        <img
+          src="./image2.png"
+          alt=""
+          style={{ width: 350, height: 300 }}
+        ></img>
+        {/* any img in the src doesn't display idk why!!! it's not related to the picture */}
+      </div>
+
+      <div>
+        <video width={320} height={240} controls>
+          <source src="/batman.mp4" type="video/mp4"></source>
+        </video>
+      </div>
+    </React.Fragment>
   );
 }
 
